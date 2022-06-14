31bc9116f8ede2bfe0a29e5d828609352c038991
Акмаль <250296a@gmail.com>
2022-06-14 13:21:34 +0600

    bishkek

---
+++ src/components/Header.vue
@@ -0,0 +1,149 @@
+<template lang="pug">
+<Menu :class="{ open: menuСondition, close: menuСlose }" />
+header.header
+  .container  
+    .col-wrap
+      .col
+        .header__logo
+          a(href="#")
+            img(v-lazy="{ src: '/public/img/logo.png' }")
+      .col
+        .header__element
+          .header__element-img
+            img(v-lazy="{ src: '/public/img/header-element.png' }")
+          .header__element-desc
+            p(v-html="headerElementDesc")
+      .col
+        .hamburger(@click="hamburger" :class="{ open: menuСondition }")
+</template>
+
+<style lang="scss" scoped>
+.header {
+  padding: 27px 0;
+
+  .col {
+    width: 33.33%;
+
+    &:nth-child(2) {
+      @include jcc;
+    }
+
+    &:last-child {
+      @include jfcfe;
+    }
+  }
+
+  &__logo {
+    max-width: 132px;
+    width: 100%;
+  }
+
+  &__element {
+    @include aistr;
+  }
+
+  &__element-img {
+    max-width: 150px;
+    width: 100%;
+    @include aic;
+  }
+
+  &__element-img,
+  &__element-desc {
+    padding: 0 15px;
+  }
+
+  &__element-desc {
+    border-left: 1px solid $wc;
+    @include aic;
+
+    p {
+      font-weight: 300;
+      color: $wc;
+      font-family: $oswald;
+      line-height: 23.71px;
+    }
+  }
+}
+
+.hamburger {
+  position: relative;
+  width: 40px;
+  height: 30px;
+  @include aic;
+  cursor: pointer;
+
+  &:hover {
+    &:before {
+      width: 32px;
+    }
+
+    &:after {
+      width: 100%;
+    }
+  }
+
+  &:before,
+  &:after {
+    position: absolute;
+    content: '';
+    height: 2px;
+    background-color: $wc;
+    transition: $tr;
+    right: 0;
+  }
+
+  &:before {
+    width: 100%;
+    margin-bottom: 8px;
+  }
+
+  &:after {
+    width: 32px;
+    margin-top: 8px;
+  }
+
+  &.open {
+
+    &:before,
+    &:after {
+      width: 100%;
+      margin: 0;
+    }
+
+    &:before {
+      transform: rotate(45deg);
+    }
+
+    &:after {
+      transform: rotate(-45deg);
+    }
+  }
+}
+</style>
+
+<script >
+import Menu from '@/components/Menu.vue';
+export default {
+  components: {
+    Menu
+  },
+  data() {
+    return {
+      headerElementDesc: 'РАЗРАБОТКА САЙТОВ <br> В КЫРГЫЗСТАНЕ',
+      menuСondition: false,
+      menuСlose: false,
+    }
+  },
+  methods: {
+    hamburger() {
+      this.menuСondition = !this.menuСondition;
+      if (this.menuСondition == false) {
+        this.menuСlose = true
+      } else {
+        this.menuСlose = false
+      }
+    }
+  }
+}
+</script>
---
+++ src/components/Menu.vue
@@ -0,0 +1,162 @@
+<template lang="pug">
+.menu
+  .menu__wrap.container
+    .menu__top
+      ul
+        li(v-for="item in nav")
+          a(href="#" @mouseenter="item = false") {{ item.item }}  
+      ul
+        li(v-for="item in subMenu")
+          a(href="#") {{ item.item }}      
+    .menu__bottom 
+
+
+</template>
+
+<style lang="scss">
+.menu {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: $bc;
+  transform: translateY(-100%);
+  opacity: 0;
+
+  &__wrap {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    height: 100%;
+  }
+
+  &__top {
+    @include aifs-jfcsb;
+    width: 100%;
+
+    ul {
+      padding: 0;
+
+      &:last-child {
+        text-align: right;
+      }
+
+      li {
+        margin-bottom: 35px;
+
+        &:last-child {
+          margin-bottom: 0;
+        }
+
+        * {
+          text-transform: uppercase;
+          color: $wc;
+          font-size: 54px;
+          line-height: 54px;
+          font-weight: 200;
+          text-decoration: none;
+          font-family: $jost;
+          // -webkit-text-stroke: 1px $wc;
+          // color: transparent;
+        }
+      }
+    }
+  }
+
+  &.open {
+    animation-duration: .5s;
+    animation-fill-mode: both;
+    animation-name: bounce;
+  }
+
+  &.close {
+    animation-duration: .5s;
+    animation-fill-mode: both;
+    animation-name: close;
+  }
+
+  @keyframes bounce {
+    0% {
+      transform: translateY(-100%);
+      opacity: 1;
+    }
+
+    50% {
+      transform: translateY(100px) scaleY(0.8);
+      opacity: 1;
+    }
+
+    100% {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes close {
+    0% {
+      transform: translateY(0);
+      opacity: 1;
+    }
+
+    100% {
+      transform: translateY(-100%);
+      opacity: 0;
+    }
+  }
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      nav: [
+        {
+          item: 'Главная',
+          link: '#',
+        },
+        {
+          item: 'Услуги',
+          link: '#',
+        },
+        {
+          item: 'Портфолио',
+          link: '#',
+        },
+        {
+          item: 'О компании',
+          link: '#',
+        },
+        {
+          item: 'Блог',
+          link: '#',
+        },
+        {
+          item: 'Магазин',
+          link: '#',
+        },
+      ],
+      subMenu: [
+        {
+          item: 'разработка сайтов',
+          link: '#',
+        },
+        {
+          item: 'iT-аутсорс',
+          link: '#',
+        },
+        {
+          item: 'разработка дизайна',
+          link: '#',
+        },
+        {
+          item: 'интернет маркетинг',
+          link: '#',
+        },
+        {
+          item: 'консалтинг',
+          link: '#'
+        },
+      ]
+    }
+  }
+}
+</script>
